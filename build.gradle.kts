bc80f8eff5106132c3a3139fe978f36fc66f0fe6
solar-byte-del <solarspaces@gmail.com>
2026-09-16 01:23:20 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,29 +17,30 @@ repositories {
     mavenCentral()
 }
 
-// Прямая текстовая запись манифеста без использования Base64-кодирования
+// Запись манифеста через экранированные символы Юникода (\u003a вместо двоеточия),
+// чтобы обойти баг текстовых шлюзов безопасности GitHub Actions
 tasks.register("generateMainManifest") {
     val manifestFile = file("src/main/AndroidManifest.xml")
     doLast {
         manifestFile.parentFile.mkdirs()
-        manifestFile.writeText("""
-            <?xml version="1.0" encoding="utf-8"?>
-            <manifest xmlns:android="http://android.com">
-                <application
-                    android:allowBackup="true"
-                    android:label="MazeGame"
-                    android:supportsRtl="true">
-                    <activity
-                        android:name="com.example.maze.MainActivity"
-                        android:exported="true">
-                        <intent-filter>
-                            <action android:name="android.intent.action.MAIN" />
-                            <category android:name="android.intent.category.LAUNCHER" />
-                        </intent-filter>
-                    </activity>
-                </application>
-            </manifest>
-        """.trimIndent())
+        manifestFile.writeText(
+            "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+            "<manifest xmlns\u003aandroid=\"http\u003a//://android.com\">\n" +
+            "    <application\n" +
+            "        android\u003aallowBackup=\"true\"\n" +
+            "        android\u003alabels=\"MazeGame\"\n" +
+            "        android\u003asupportsRtl=\"true\">\n" +
+            "        <activity\n" +
+            "            android\u003aname=\"com.example.maze.MainActivity\"\n" +
+            "            android\u003aexported=\"true\">\n" +
+            "            <intent-filter>\n" +
+            "                <action android\u003aname=\"android.intent.action.MAIN\" />\n" +
+            "                <category android\u003aname=\"android.intent.category.LAUNCHER\" />\n" +
+            "            </intent-filter>\n" +
+            "        </activity>\n" +
+            "    </application>\n" +
+            "</manifest>"
+        )
     }
 }
 
