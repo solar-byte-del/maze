91cca2f972637f6151df7286c897ddf3d3cf0685
solar-byte-del <solarspaces@gmail.com>
2026-09-16 23:53:32 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,7 @@ android {
     }
 
     composeOptions {
-        // Версия компилятора под стабильную ветку 1.5.x
-        kotlinCompilerExtensionVersion = "1.5.4"
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
 
     compileOptions {
@@ -46,13 +45,14 @@ android {
     }
 }
 
-// Поставили сверхстабильную ветку 1.5.4, которая на 100% есть во всех репозиториях Google
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
     
-    implementation("androidx.compose.ui:compose-ui:1.5.4")
-    implementation("androidx.compose.ui:compose-ui-graphics:1.5.4")
-    implementation("androidx.compose.material3:material3:1.1.2")
+    // Подключаем официальную платформу управления версиями (BOM), как в шахматах
+    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
+    implementation("androidx.compose.ui:compose-ui")
+    implementation("androidx.compose.ui:compose-ui-graphics")
+    implementation("androidx.compose.material3:material3")
 }
