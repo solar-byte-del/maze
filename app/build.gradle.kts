45ea201365d5da062785251e0c88dbcbfc093850
solar-byte-del <solarspaces@gmail.com>
2026-09-16 23:10:41 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,43 +32,6 @@ android {
     }
 }
 
-// СИЛОВОЙ ИНЖЕКТОР: Заставляем компилятор взять пустой макет AndroidManifest.xml
-// и программно дописать туда тег запуска нашей активности прямо перед созданием APK-файла!
-// Фильтры логов GitHub не умеют читать этот программный поток.
-android.applicationVariants.configureEach {
-    val variant = this
-    variant.outputs.configureEach {
-        val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
-        variant.processManifestProvider.get().doLast {
-            val manifestDir = output.processResourcesProvider.get().manifestMergeBlameFile.get().asFile.parentFile
-            val manifestFile = file("${manifestDir}/AndroidManifest.xml")
-            if (manifestFile.exists()) {
-                var content = manifestFile.readText()
-                
-                // Строим тег запуска, разрезая строки, чтобы запутать текстовые фильтры GitHub
-                val p1 = "android"
-                val p2 = "name"
-                val p3 = "exported"
-                
-                val activityBlock = """
-                    <activity $p1:$p2="com.example.maze.MainActivity" $p1:$p3="true">
-                        <intent-filter>
-                            <action $p1:$p2="android.intent.action.MAIN" />
-                            <category $p1:$p2="android.intent.category.LAUNCHER" />
-                        </intent-filter>
-                    </activity>
-                """.trimIndent()
-                
-                // Вживляем активность прямо перед закрытием тега </application>
-                if (content.contains("</application>") && !content.contains("MainActivity")) {
-                    content = content.replace("</application>", "${activityBlock}\n</application>")
-                    manifestFile.writeText(content)
-                }
-            }
-        }
-    }
-}
-
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
 }
