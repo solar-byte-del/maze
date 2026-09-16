d759f86895870e5061fa0def3e809cd4fc147f83
solar-byte-del <solarspaces@gmail.com>
2026-09-16 21:55:24 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,62 +1,23 @@
-import java.util.Base64
-
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
+        // Версия сборщика, полностью оптимизированная под современные стандарты
         classpath("com.android.tools.build:gradle:8.3.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     }
 }
 
-apply(plugin = "com.android.application")
-apply(plugin = "org.jetbrains.kotlin.android")
-
-repositories {
-    google()
-    mavenCentral()
+plugins {
+    id("com.android.application") apply false
+    id("org.jetbrains.kotlin.android") apply false
 }
 
-// УЛЬТРА-ХАК: Идеальный манифест лабиринта, завязанный строго на пакет com.example.maze
-tasks.register("generateMainManifest") {
-    val manifestFile = file("src/main/AndroidManifest.xml")
-    doLast {
-        manifestFile.parentFile.mkdirs()
-        val base64Manifest = "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPG1hbmlmZXN0IHhtbG5zOmFuZHJvaWQ9Imh0dHA6Ly9zY2hlbWFzLmFuZHJvaWQuY29tL2Fway9yZXMvYW5kcm9pZCI+CiAgICA8YXBwbGljYXRpb24KICAgICAgICBhbmRyb2lkOmFsbG93QmFja3VwPSJ0cnVlIgogICAgICAgIGFuZHJvaWQ6bGFiZWw9Ik1hemVHYW1lIgogICAgICAgIGFuZHJvaWQ6c3VwcG9ydHNSdGw9InRydWUiPgogICAgICAgIDxhY3Rpdml0eQogICAgICAgICAgICBhbmRyb2lkOm5hbWU9ImNvbS5leGFtcGxlLm1hemUuTWFpbkFjdGl2aXR5IgogICAgICAgICAgICBhbmRyb2lkOmV4cG9ydGVkPSJ0cnVlIj4KICAgICAgICAgICAgPGludGVudC1maWx0ZXI+CiAgICAgICAgICAgICAgICA8YWN0aW9uIGFuZHJvaWQ6bmFtZT0iYW5kcm9pZC5pbnRlbnQuYWN0aW9uLk1BSU4iIC8+CiAgICAgICAgICAgICAgICA8Y2F0ZWdvcnkgYW5kcm9pZDpuYW1lPSJhbmRyb2lkLmludGVudC5jYXRlZ29yeS5MQVVOQ0hFUiIgLz4KICAgICAgICAgICAgPC9pbnRlbnQtZmlsdGVyPgogICAgICAgIDwvYWN0aXZpdHk+CiAgICA8L2FwcGxpY2F0aW9uPgo8L21hbmlmZXN0Pg=="
-        val decodedBytes = Base64.getDecoder().decode(base64Manifest)
-        manifestFile.writeBytes(decodedBytes)
+subprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
-}
-
-tasks.configureEach {
-    if (name.startsWith("process") && name.contains("Manifest")) {
-        dependsOn("generateMainManifest")
-    }
-}
-
-configure<com.android.build.gradle.AppExtension> {
-    // Жестко фиксируем системный адрес приложения
-    namespace = "com.example.maze"
-    compileSdkVersion(34)
-
-    defaultConfig {
-        applicationId = "com.example.maze"
-        minSdkVersion(21) // Расширили совместимость до Android 5.0+ для исключения крашей на старых прошивках
-        targetSdkVersion(34)
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-}
-
-dependencies {
-    "implementation"("androidx.core:core-ktx:1.12.0")
 }
