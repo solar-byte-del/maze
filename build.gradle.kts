3bf8ffa276b64f61ca2147a8dcca277f16db2f9f
solar-byte-del <solarspaces@gmail.com>
2026-09-16 12:38:09 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,6 @@ repositories {
 }
 
 configure<com.android.build.gradle.AppExtension> {
-    // Выставляем правильное пространство имен игры лабиринта
     namespace = "com.example.maze"
     compileSdkVersion(34)
 
@@ -35,38 +34,6 @@ configure<com.android.build.gradle.AppExtension> {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
-    }
-}
-
-// АВТОМАТИЧЕСКОЕ ИНЖЕКТИРОВАНИЕ: Заставляем ядро Android Gradle Plugin самостоятельно
-// создать и прописать MainActivity в манифест внутри памяти сервера прямо перед упаковкой APK
-androidComponents {
-    onVariants { variant ->
-        variant.artifacts.use(
-            tasks.register("createManifestAutomatically") {
-                val outputManifest = file("src/main/AndroidManifest.xml")
-                doLast {
-                    outputManifest.parentFile.mkdirs()
-                    // Разбиваем системное слово "android:name", чтобы обойти фильтры логов GitHub
-                    val p1 = "android"
-                    val p2 = "name"
-                    val p3 = "exported"
-                    outputManifest.writeText("""
-                        <?xml version="1.0" encoding="utf-8"?>
-                        <manifest xmlns:$p1="http://android.com">
-                            <application $p1:allowBackup="true" $p1:label="MazeGame" $p1:supportsRtl="true">
-                                <activity $p1:$p2="com.example.maze.MainActivity" $p1:$p3="true">
-                                    <intent-filter>
-                                        <action $p1:$p2="android.intent.action.MAIN" />
-                                        <category $p1:$p2="android.intent.category.LAUNCHER" />
-                                    </intent-filter>
-                                </activity>
-                            </application>
-                        </manifest>
-                    """.trimIndent())
-                }
-            }
-        ).wiredWith { it }
     }
 }
 
