dab3d13e085d24c0b9de803317c14d35fe54bc36
solar-byte-del <solarspaces@gmail.com>
2026-09-16 22:33:22 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,41 +3,6 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-// УЛЬТРА-ХАК: Посимвольное и покомпонентное склеивание строк полностью скрывает
-// двоеточия от систем отслеживания логов GitHub, гарантируя сборку манифеста
-tasks.register("generateRealManifest") {
-    val manifestFile = file("src/main/AndroidManifest.xml")
-    doLast {
-        manifestFile.parentFile.mkdirs()
-        
-        val a = "android"
-        val n = "name"
-        val e = "exported"
-        val t = "theme"
-        
-        val sb = StringBuilder()
-        sb.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
-        sb.append("<manifest xmlns:$a=\"http://android.com\">\n")
-        sb.append("    <application $a:allowBackup=\"true\" $a:label=\"MazeGame\" $a:$t=\"@style/Theme.AppCompat.Light.NoActionBar\" $a:supportsRtl=\"true\">\n")
-        sb.append("        <activity $a:$n=\"com.example.maze.MainActivity\" $a:$e=\"true\">\n")
-        sb.append("            <intent-filter>\n")
-        sb.append("                <action $a:$n=\"android.intent.action.MAIN\" />\n")
-        sb.append("                <category $a:$n=\"android.intent.category.LAUNCHER\" />\n")
-        sb.append("            </intent-filter>\n")
-        sb.append("        </activity>\n")
-        sb.append("    </application>\n")
-        sb.append("</manifest>")
-        
-        manifestFile.writeText(sb.toString())
-    }
-}
-
-tasks.configureEach {
-    if (name.startsWith("process") && name.contains("Manifest")) {
-        dependsOn("generateRealManifest")
-    }
-}
-
 android {
     namespace = "com.example.maze"
     compileSdk = 34
